bd30991711157b19af6d73aaa5c7402d041ecba4
jagannath sahoo <100899474+Tiger9937@users.noreply.github.com>
2024-11-20 13:11:16 -0800

    Build Loging system mini

--- Atrixed/src/Atixed/EntryPoint.h
+++ Atrixed/src/Atixed/EntryPoint.h
@@ -4,12 +4,17 @@
 #ifdef AX_PLATFORM_WINDOWS 
 
 namespace Atrixed {
-    extern App* CreateApplication(); // Declaration of CreateApplication function
+    extern Atrixed::App* CreateApplication();  
 }
 
-int main(int argc, char** argv) {
 
+int main(int argc, char** argv) {
     
+    Atrixed::Log::Init();
+
+    AX_CORE_WARN("Loging now rasy");
+    AX_CLIENT_FLUSH();
+
    
     auto app = Atrixed::CreateApplication();
     app->Run();
@@ -17,3 +22,5 @@ int main(int argc, char** argv) {
 }
 
 #endif // AX_PLATFORM_WINDOWS
+
+
--- Atrixed/src/Atrixed.h
+++ Atrixed/src/Atrixed.h
@@ -3,6 +3,9 @@
 #include "Atixed/App.h"
 
 
+// loging 
+ #include "Atixed/log.h"
+
 
 #include "Atixed/EntryPoint.h"
 
